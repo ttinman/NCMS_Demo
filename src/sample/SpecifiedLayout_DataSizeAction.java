635b85798f4d1672ef4ccb0c5efa68debff051d1
unknown <Ngan Trung@.(none)>
2011-06-01 22:45:11 +0700

    Demo

--- src/sample/SpecifiedLayout_DataSizeAction.java
+++ src/sample/SpecifiedLayout_DataSizeAction.java
@@ -9,18 +9,24 @@ import prefuse.action.assignment.ColorAction;
 import prefuse.action.assignment.DataSizeAction;
 import prefuse.action.layout.Layout;
 import prefuse.action.layout.SpecifiedLayout;
+import prefuse.action.layout.graph.ForceDirectedLayout;
 import prefuse.activity.Activity;
 import prefuse.controls.*;
 import prefuse.data.Graph;
+import prefuse.data.Node;
 import prefuse.data.io.GraphMLReader;
+import prefuse.data.io.GraphMLWriter;
 import prefuse.render.DefaultRendererFactory;
 import prefuse.render.LabelRenderer;
 import prefuse.util.ColorLib;
+import prefuse.visual.NodeItem;
 import prefuse.visual.VisualItem;
 
 import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.util.Iterator;
 
 /**
  * This small demo shall demonstrate some simple issues:
@@ -69,7 +75,7 @@ public class SpecifiedLayout_DataSizeAction extends JPanel {
         };
         printing.setStepTime(1000); // seems not to work, don't know why. Bug?
         
-        ActionList draw = new ActionList(Activity.INFINITY);
+        ActionList draw = new ActionList(1000);
         // commented out because it produces much output to console
         //   remove comments to see 3) in "action"
 //        draw.add(printing);  
@@ -77,7 +83,18 @@ public class SpecifiedLayout_DataSizeAction extends JPanel {
         draw.add(specifiedLayout);  
         draw.add(dataSizeAction);  
         draw.add(new RepaintAction());
-        m_vis.putAction("draw", draw);  
+        m_vis.putAction("draw", draw);
+
+        Layout specifiedLayou1 = new ForceDirectedLayout("graph.nodes");
+        ActionList draw1 = new ActionList(Activity.INFINITY);
+        // commented out because it produces much output to console
+        //   remove comments to see 3) in "action"
+//        draw.add(printing);
+        draw1.add(coloring);
+        draw1.add(specifiedLayou1);
+        draw1.add(dataSizeAction);
+        draw1.add(new RepaintAction());
+        m_vis.putAction("draw1", draw1);
         
         // --------------------------------------------------------------------
         // set up a display to show the visualization
@@ -85,6 +102,7 @@ public class SpecifiedLayout_DataSizeAction extends JPanel {
         Display display = new Display(m_vis);
         display.setSize(300,300);
         display.pan(50,50);
+        display.addControlListener(new DragControl());
         display.addControlListener(new PanControl());
         display.addControlListener(new ZoomControl());
         display.addControlListener(new WheelZoomControl());
@@ -93,7 +111,7 @@ public class SpecifiedLayout_DataSizeAction extends JPanel {
 
         // now we run our action list
         m_vis.run("draw");
-        
+        m_vis.run("draw1");
         add(display);
     }
     
@@ -115,11 +133,32 @@ public class SpecifiedLayout_DataSizeAction extends JPanel {
         // 3) how to stop a running action if prefuse window is deactivated
         frame.addWindowListener(new WindowAdapter() {
             public void windowActivated(WindowEvent e) {
-                view.m_vis.run("draw");
+//                view.m_vis.run("draw");
+                 System.out.println("windowActivated");
             }
             public void windowDeactivated(WindowEvent e) {
-                view.m_vis.cancel("draw");
+//                view.m_vis.cancel("draw");
+                 System.out.println("windowDeactivated");
+
             }
+             public void windowClosing(WindowEvent e) {
+                 try{
+                     System.out.println("windowClosed");
+                      Graph graph = (Graph)view.m_vis.getSourceData("graph");
+                     Iterator ite = view.m_vis.getGroup(nodes).tuples();
+                     while(ite.hasNext()){
+                         NodeItem node = (NodeItem)ite.next();
+                         node.set("x-coord",node.getX());
+                         node.set("y-coord",node.getY());
+                     }
+                      new GraphMLWriter().writeGraph(
+(Graph)view.m_vis.getSourceData("graph"),
+new File("datasets/graphml-sample2.xml"));
+                 }catch (Exception ex){
+                     ex.printStackTrace();
+                 }
+
+             }
         });
     }
     
